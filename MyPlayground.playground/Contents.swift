2bda1585af2738c67d3529a4f82633a3d20a8842
change4ready <faithit2success@gmail.com>
2023-01-20 11:48:11 -0500

    adding day1 and day2

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,34 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphne"
+
+var playerName = "Roy"
+print(playerName)
+
+playerName = "Dani"
+
+print(playerName)
+
+playerName = "Sam"
+print(playerName)
+
+let managerName = "Michael Scott"
+let dogBreed = "Samoyed"
+let meaningOfLife = "How many roads must a man walk down?"
+
+
+
+let number = 0.1 + 0.2
+print(number)
+
+let a = 1
+let b = 2.0
+
+
+let c = a + Int(b)
---
+++ day2/string_interpolation.playground/Contents.swift
@@ -0,0 +1,33 @@
+import Cocoa
+
+let firstPart = "Hello, "
+let secondPart = "world!"
+let greeting = firstPart + secondPart
+
+let people = "Haters"
+let action = "hate"
+let lyric = people + " gonna " + action
+print(lyric)
+
+let luggageCode = "1" + "2" + "3" + "4" + "5"
+
+let quote = "Then he tapped a sign saying \"Believe\" and walked away."
+
+let name = "Taylor"
+let age = 26
+let message = "Hello, my name is \(name) and I'm \(age) years old."
+print(message)
+
+let number = 11
+let missionMessage = "Apollo " + String(number) + " landed on the moon."
+
+// Challenge = Checkpoint 1
+// Creata a constant holding any temperature in Celsius.
+// Convert that temperature to Fahrenheit by multiplying by 9, dividing by 5, then adding 32.
+// Prints the result, showing both the Celsius and Fahrenheit values.
+
+
+let celsius = 35
+let fahrenheit = (celsius * 9) / 5 + 32
+print("\(celsius) degrees cesius is equal to \(fahrenheit) degrees fahrenheit.")
+
