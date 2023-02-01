a5a72df30d78229ed020ff17e616ab52c0cf67d0
change4ready <faithit2success@gmail.com>
2023-01-31 20:48:33 -0500

    adding completed day 8

---
+++ day8/default_parameters.playground/Contents.swift
@@ -0,0 +1,25 @@
+import Cocoa
+
+func printTimesTables(_ number: Int, end: Int = 12) {
+    for i in 1...end {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTables(5, end: 20)
+printTimesTables(8)
+
+
+func printTimesTables(for number: Int, end: Int = 12) {
+    for i in 1...end {
+        print("\(i) x \(number) is \(i * number)")
+    }
+}
+
+printTimesTables(for: 5, end: 20)
+printTimesTables(for: 8)
+
+var characters = ["Lana", "Pam", "Ray", "Sterling"]
+print(characters.count)
+characters.removeAll()
+print(characters.count)
